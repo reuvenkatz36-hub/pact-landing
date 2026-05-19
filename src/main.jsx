69f519851fb8bfb9d2ea97cf6eaddd94fa2be3a1
reuvenkatz36-hub <reuvenkatz36@gmail.com>
2026-05-19 20:07:36 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PactLanding from './PactLanding.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PactLanding />
+  </React.StrictMode>
+)
